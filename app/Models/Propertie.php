223dd85f8073d6dc207de82bd9c3c3265d2b2e81
sneha <snehapalle12@gmail.com>
2024-11-19 21:45:53 +0530

    added fields

--- app/Models/Propertie.php
+++ app/Models/Propertie.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Propertie extends Model
 {
-    //
+    protected $table = 'properties';
+
+    protected $fillable = [
+        'title', 'subtitle', 'name', 'country', 'state', 'district', 'city', 'local_city',
+        'facing', 'bathrooms', 'bedrooms', 'start_sqft', 'end_sqft', 'start_price', 'end_price',
+        'no_floors', 'parking', 'estimated_emi'
+    ];
+
 }
