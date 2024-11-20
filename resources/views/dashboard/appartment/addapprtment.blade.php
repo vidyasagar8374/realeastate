5acb28f84ade0bfa10f8e6af55c819e555d8d073
sneha <snehapalle12@gmail.com>
2024-11-20 22:31:53 +0530

    added values

--- resources/views/dashboard/appartment/addapprtment.blade.php
+++ resources/views/dashboard/appartment/addapprtment.blade.php
@@ -56,19 +56,19 @@
                 <div class="row">
                   <div class="col-lg-4">
                     <div class="form-floating mb-3">
-                      <input type="text" class="form-control" id="floatingInput" mailto:placeholder="name@example.com">
+                      <input type="text" id="title" name="title"  value="{{ old('title') }}"  class="form-control" id="floatingInput" mailto:placeholder="name@example.com">
                       <label for="floatingInput">Title</label>
                     </div>
                   </div>
                   <div class="col-lg-4">
                     <div class="form-floating mb-3">
-                      <input type="text" class="form-control" id="floatingtext" placeholder="Description">
+                      <input type="text" id="subtitle" name="subtitle"  value="{{ old('subtitle') }}" class="form-control" id="floatingtext" placeholder="Description">
                       <label for="floatingPassword">Subtitle</label>
                     </div>
                   </div>
                   <div class="col-lg-4">
                     <div class="form-floating mb-3">
-                      <input type="text" class="form-control" id="floatingtext" placeholder="Description">
+                      <input type="text"  id="name" name="name"  value="{{ old('name') }}" class="form-control" id="floatingtext" placeholder="Description">
                       <label for="floatingPassword">Name</label>
                     </div>
                   </div>
@@ -76,9 +76,9 @@
                     <div class="form-floating mb-3">
 
                         <select class="form-control" id="country" name="country">
-                          <option value="" disabled selected>Select Country</option>
-                          <option value="United States">United States</option>
-                          <option value="Canada">Canada</option>
+                          <option value="" disabled {{ old('country') ? '' : 'selected' }}>Select Country</option>
+                          <option value="United States" {{ old('country') == 'United States' ? 'selected' : '' }}>United States</option>
+                          <option value="Canada"{{ old('country') == 'Canada' ? 'selected' : '' }}>Canada</option>
                           <option value="United Kingdom">United Kingdom</option>
                           <option value="India">India</option>
                           <option value="Australia">Australia</option>
@@ -94,7 +94,7 @@
 
                   <div class="col-lg-4">
                     <div class="form-floating mb-3">
-                      <select class="form-control" id="country" name="country">
+                      <select class="form-control" id="country" name="country"  value="{{ old('bathrooms') }}" >
                           <option value="" disabled selected>Select State</option>
                           <option value="United States">United States</option>
                           <option value="Canada">Canada</option>
@@ -162,61 +162,61 @@
                   </div>
                   <div class="col-lg-4">
                     <div class="form-floating mb-3">
-                      <input type="text" class="form-control" id="floatingtext" placeholder="Description">
+                      <input type="text" id="facing" name="facing" value="{{ old('facing') }}" class="form-control" id="floatingtext" placeholder="Description">
                       <label for="floatingPassword">Facing</label>
                     </div>
                   </div>
                   <div class="col-lg-4">
                     <div class="form-floating mb-3">
-                      <input type="text" class="form-control" id="floatingtext" placeholder="Description">
+                      <input type="text" id="bathrooms" name="bathrooms" value="{{ old('bathrooms') }}" class="form-control" id="floatingtext" placeholder="Description">
                       <label for="floatingPassword">Bathrooms</label>
                     </div>
                   </div>
                   <div class="col-lg-4">
                     <div class="form-floating mb-3">
-                      <input type="text" class="form-control" id="floatingtext" placeholder="Description">
+                      <input type="text" id="bedrooms" name="bedrooms" value="{{ old('bedrooms') }}" class="form-control" id="floatingtext" placeholder="Description">
                       <label for="floatingPassword">Bedrooms</label>
                     </div>
                   </div>
                   <div class="col-lg-4">
                     <div class="form-floating mb-3">
-                      <input type="text" class="form-control" id="floatingtext" placeholder="Description">
+                      <input type="text" id="start_sqft" name="start_sqft" value="{{ old('start_sqft') }}" class="form-control" id="floatingtext" placeholder="Description">
                       <label for="floatingPassword">Startingsqft</label>
                     </div>
                   </div>
                   <div class="col-lg-4">
                     <div class="form-floating mb-3">
-                      <input type="text" class="form-control" id="floatingtext" placeholder="Description">
+                      <input type="text" id="end_sqft" name="end_sqft" value="{{ old('end_sqft') }}" class="form-control" id="floatingtext" placeholder="Description">
                       <label for="floatingPassword">Endsqft</label>
                     </div>
                   </div>
                   <div class="col-lg-4">
                     <div class="form-floating mb-3">
-                      <input type="text" class="form-control" id="floatingtext" placeholder="Description">
+                      <input type="text" id="start_price" name="start_price" value="{{ old('start_price') }}" class="form-control" id="floatingtext" placeholder="Description">
                       <label for="floatingPassword">Startprice</label>
                     </div>
                   </div>
                   <div class="col-lg-4">
                     <div class="form-floating mb-3">
-                      <input type="text" class="form-control" id="floatingtext" placeholder="Description">
+                      <input type="text" id="end_price" name="end_price" value="{{ old('end_price') }}" class="form-control" id="floatingtext" placeholder="Description">
                       <label for="floatingPassword">Endprice</label>
                     </div>
                   </div>
                   <div class="col-lg-4">
                     <div class="form-floating mb-3">
-                      <input type="text" class="form-control" id="floatingtext" placeholder="Description">
+                      <input type="text" id="no_floors" name="no_floors" value="{{ old('no_floors') }}" class="form-control" id="floatingtext" placeholder="Description">
                       <label for="floatingPassword">Nofloors</label>
                     </div>
                   </div>
                   <div class="col-lg-4">
                     <div class="form-floating mb-3">
-                      <input type="text" class="form-control" id="floatingtext" placeholder="Description">
+                      <input type="text" id="parking" name="parking" value="{{ old('parking') }}" class="form-control" id="floatingtext" placeholder="Description">
                       <label for="floatingPassword">Parking</label>
                     </div>
                   </div>
                   <div class="col-lg-4">
                     <div class="form-floating mb-3">
-                      <input type="text" class="form-control" id="floatingtext" placeholder="Description">
+                      <input type="text" id="estimated_emi" name="estimated_emi" value="{{ old('estimated_emi') }}" class="form-control" id="floatingtext" placeholder="Description">
                       <label for="floatingPassword">Estimated Emi</label>
                     </div>
                   </div>
